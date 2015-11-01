24916a9b041279a54de1b430a472cf25a1247843
Debabrata Tripathy <debabratatripathy@Debabratas-MacBook-Air.local>
2015-11-01 19:36:11 +0530

    now supported tsp and jstl

--- java-ee/src/main/java/edu/poc/javaee/web/servlet/SampleServlet.java
+++ java-ee/src/main/java/edu/poc/javaee/web/servlet/SampleServlet.java
@@ -1,16 +1,22 @@
 package edu.poc.javaee.web.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.poc.javaee.web.jstl.Employee;
+
 /**
  * Servlet implementation class HelloWorld
  */
-@WebServlet("/")
+@WebServlet("/jstl")
 public class SampleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,7 +32,19 @@ public class SampleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().println("Hello World!!!");
+		List<Employee> empList = new ArrayList<Employee>();
+        Employee emp1 = new Employee();
+        emp1.setId(1); emp1.setName("Pankaj");emp1.setRole("Developer");
+        Employee emp2 = new Employee();
+        emp2.setId(2); emp2.setName("Meghna");emp2.setRole("Manager");
+        empList.add(emp1);empList.add(emp2);
+        request.setAttribute("empList", empList);
+         System.out.println(empList);
+        request.setAttribute("htmlTagData", "<br/> creates a new line.");
+        request.setAttribute("url", "http://www.ok.com");
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
+        rd.forward(request, response);
+    
 	}
 
 	/**
---
+++ java-ee/src/main/webapp/customTag.jsp
@@ -0,0 +1,9 @@
+<%@ taglib uri="WEB-INF/mytags.tld" prefix="m"%>
+
+<html>
+<body>
+	Current Date and Time is:
+	<m:today />
+</body>
+</html>
+
